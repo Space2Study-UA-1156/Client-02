5651973b0bf3d7f0c60aa4c6cbb384f55689d5cd
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2024-04-29 21:52:42 +0300

    add additional test for text

--- tests/unit/pages/subjects/Subjects.spec.jsx
+++ tests/unit/pages/subjects/Subjects.spec.jsx
@@ -1,9 +1,15 @@
-import { render } from '@testing-library/react'
+import { renderWithProviders } from '~tests/test-utils'
+import { render, screen } from '@testing-library/react'
 
 import Subjects from '~/pages/subjects/Subjects'
 
 describe('Subjects render test', () => {
   test('should render Subjects page', () => {
+    renderWithProviders(<Subjects />)
+  })
+  test('Check text on the Subjects page', () => {
     render(<Subjects />)
+    const textElement = screen.getByText('Subjects')
+    expect(textElement).toBeInTheDocument()
   })
 })
