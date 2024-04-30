a030c79c9309cc14f7ed5f278422f758f7f25452
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-04-30 20:08:54 +0200

    unit-test/#107-categories-page

---
+++ tests/unit/components/video-box/VideoBox.spec.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react'
+import VideoBox from '~/components/video-box/VideoBox'
+
+describe('VideoBox test', () => {
+  it('should render VideoBox component', () => {
+    render(<VideoBox />)
+  })
+  it('should render VideoBox with a particular src attribute', () => {
+    const video = 'holera/freddyfasbear.img'
+
+    render(<VideoBox video={video} />)
+    const videoElement = screen.getByAltText('Video')
+
+    expect(videoElement).toBeInTheDocument()
+    expect(videoElement).toHaveAttribute('src', video)
+  })
+})
