bff5fa16dc02b84abbe045f8e7d1dcc9084855b9
docia <matiushenko107@gmail.com>
2024-05-01 22:52:58 +0300

    update FindOffers.spec.jsx
    1. updated mock in FindOffers.spec.jsx
    2. pulled changes in package-lock. json and test-uttils.jsx

--- tests/test-utils.jsx
+++ tests/test-utils.jsx
@@ -1,22 +1,22 @@
-import { ThemeProvider } from "@mui/material/styles";
-import { configureStore } from "@reduxjs/toolkit";
-import { render } from "@testing-library/react";
-import { Provider } from "react-redux";
-import { MemoryRouter } from "react-router-dom";
-import { ConfirmationDialogProvider } from "~/context/confirm-context";
-import { ModalProvider } from "~/context/modal-context";
-import { SnackBarProvider } from "~/context/snackbar-context";
-import reducer from "~/redux/reducer";
-import { theme } from "~/styles/app-theme/custom-mui.styles";
+import { ThemeProvider } from '@mui/material/styles'
+import { configureStore } from '@reduxjs/toolkit'
+import { render } from '@testing-library/react'
+import { Provider } from 'react-redux'
+import { MemoryRouter } from 'react-router-dom'
+import { ConfirmationDialogProvider } from '~/context/confirm-context'
+import { ModalProvider } from '~/context/modal-context'
+import { SnackBarProvider } from '~/context/snackbar-context'
+import reducer from '~/redux/reducer'
+import { theme } from '~/styles/app-theme/custom-mui.styles'
 
-import MockAdapter from "axios-mock-adapter";
-import { vi } from "vitest";
-import { axiosClient } from "~/plugins/axiosClient";
+import MockAdapter from 'axios-mock-adapter'
+import { vi } from 'vitest'
+import { axiosClient } from '~/plugins/axiosClient'
 
 export const renderWithProviders = (
   ui,
   {
-    initialEntries = "/",
+    initialEntries = '/',
     preloadedState,
     store = configureStore({ reducer: { appMain: reducer }, preloadedState }),
     ...renderOptions
@@ -34,13 +34,13 @@ export const renderWithProviders = (
         </ThemeProvider>
       </MemoryRouter>
     </Provider>
-  );
-  return render(ui, { wrapper: Wrapper, ...renderOptions });
-};
+  )
+  return render(ui, { wrapper: Wrapper, ...renderOptions })
+}
 
 export const getFakeTestEvent = (key, value) => ({
   preventDefault: vi.fn(),
-  target: { [key]: value },
-});
+  target: { [key]: value }
+})
 
-export const mockAxiosClient = new MockAdapter(axiosClient);
+export const mockAxiosClient = new MockAdapter(axiosClient)
