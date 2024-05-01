026c8bc5f48394957ed202d3c6389356279c7fd1
Max <max@webmaestro.com.ua>
2024-05-01 23:00:18 +0300

    resolves conflict

---
+++ tests/unit/components/app-text-area/AppTextArea.spec.jsx
@@ -0,0 +1,66 @@
+import { render, screen } from '@testing-library/react'
+import { describe } from 'vitest'
+import AppTextArea from '~/components/app-text-area/AppTextArea'
+
+vi.mock('@mui/material/Typography', () => ({
+  _esModule: true,
+  default: (props) => {
+    return (
+      <div data-equal={`${props.value.length === props.maxLength}`}>
+        `${Number(props.value.length)}/${props.maxLength}`
+      </div>
+    )
+  }
+}))
+
+describe('AppTextArea test', () => {
+  const titleValue = 'test title'
+
+  it('should render title if props title are passed', () => {
+    render(<AppTextArea title={titleValue} />)
+    const title = screen.getByText(titleValue)
+    expect(title).toBeInTheDocument()
+  })
+
+  it("shouldn't be rendered title if props title is not passed", () => {
+    render(<AppTextArea />)
+    const title = screen.queryByText(titleValue)
+    expect(title).not.toBeInTheDocument()
+  })
+
+  it('should render value', () => {
+    const props = {
+      value: 'test value',
+      maxLength: 2
+    }
+    render(<AppTextArea {...props} />)
+    const value = screen.getByText(
+      `${Number(props.value.length)}/${props.maxLength}`
+    )
+    expect(value).toBeInTheDocument()
+  })
+
+  it("shouldn't render value", () => {
+    const props = {
+      value: 'test value',
+      maxLength: 0
+    }
+    render(<AppTextArea {...props} />)
+    const value = screen.queryByText(
+      `${Number(props.value.length)}/${props.maxLength}`
+    )
+    expect(value).not.toBeInTheDocument()
+  })
+
+  it('should render color error', () => {
+    const props = {
+      value: 'test value',
+      maxLength: 10
+    }
+    render(<AppTextArea {...props} />)
+    const value = screen.queryByText(
+      `${Number(props.value.length)}/${props.maxLength}`
+    )
+    expect(value).toHaveProperty('data-equal', 'true')
+  })
+})
