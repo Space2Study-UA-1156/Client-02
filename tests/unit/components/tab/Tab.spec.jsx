41c767fdddb1d7b62d6b4eefaeeb04b4a13a18fe
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2024-05-02 20:28:34 +0300

    mock Button component

--- tests/unit/components/tab/Tab.spec.jsx
+++ tests/unit/components/tab/Tab.spec.jsx
@@ -1,4 +1,3 @@
-import Button from '@mui/material/Button'
 import { render, screen, fireEvent } from '@testing-library/react'
 
 import Tab from '~/components/tab/Tab'
@@ -6,25 +5,30 @@ import Tab from '~/components/tab/Tab'
 const children = 'Test text'
 const handleTab = vi.fn()
 
-vi.mock('@mui/material/Button', () => {
-  return {
-    __deModule: true,
-    default: vi.fn(() => {
-      return <Button>Looks like it does not work</Button>
-    })
-  }
-})
+vi.mock('@mui/material/Button', () => ({
+  default: ({ sx, children }) => (
+    <button data-testid='button' onClick={handleTab} style={sx}>
+      {children}
+    </button>
+  )
+}))
 
 describe('Tab component test', () => {
-  test('should render Tab component with children', () => {
-    render(<Tab>{children}</Tab>)
+  it('should render Tab component with children', () => {
+    render(<Tab activeTab>{children}</Tab>)
     expect(screen.getByText(children)).toBeInTheDocument()
   })
-  test('should render Tab component with onClick', () => {
+  it('should render Tab component with onClick', () => {
     render(<Tab onClick={handleTab}>{children}</Tab>)
     const tabButton = screen.getByRole('button')
 
     fireEvent.click(tabButton)
     expect(handleTab).toHaveBeenCalled()
+  })
+  it('should render MUI AppButton properly', () => {
+    render(<Tab onClick={handleTab}>{children}</Tab>)
+
+    const appButton = screen.getByTestId('button')
+    expect(appButton).toBeInTheDocument()
   })
 })
