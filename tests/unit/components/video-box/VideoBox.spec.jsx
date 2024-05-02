0027c339d1cce1320e5f152adfb5efb7b3087245
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-05-02 09:45:42 +0200

    deleted unnecessary it

--- tests/unit/components/video-box/VideoBox.spec.jsx
+++ tests/unit/components/video-box/VideoBox.spec.jsx
@@ -2,9 +2,6 @@ import { render, screen } from '@testing-library/react'
 import VideoBox from '~/components/video-box/VideoBox'
 
 describe('VideoBox test', () => {
-  it('should render VideoBox component', () => {
-    render(<VideoBox />)
-  })
   it('should render VideoBox with a particular src attribute', () => {
     const video = 'bublyk/test.img'
 
