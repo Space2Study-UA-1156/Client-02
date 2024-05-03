fda8ace9c11d25ecfac37024be2985bc29deb9e1
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-05-03 23:32:06 +0200

    Get by text case was handled #100

--- tests/unit/pages/categories/Categories.spec.jsx
+++ tests/unit/pages/categories/Categories.spec.jsx
@@ -1,20 +1,25 @@
 import Categories from '~/pages/categories/Categories'
 import { screen } from '@testing-library/react'
 import { renderWithProviders } from '~tests/test-utils'
+import { expect } from 'vitest'
 
 vi.mock('~/components/page-wrapper/PageWrapper', () => ({
   __esModule: true,
-  default: vi.fn(() => <div data-testid='test'>Categories</div>)
+  default: vi.fn(({ children }) => {
+    return <div data-testid='test'>{children}</div>
+  })
 }))
 
 describe('Categories page test', () => {
   it('should render the categories page', () => {
     renderWithProviders(<Categories />)
+    const textElement = screen.getByText('Categories')
+    expect(textElement).toBeInTheDocument()
   })
 
   it('should render the text on the page', () => {
     renderWithProviders(<Categories />)
-    const textElement = screen.getByTestId('test')
-    expect(textElement).toHaveTextContent('Categories')
+    const categoriesElement = screen.getByTestId('test')
+    expect(categoriesElement).toBeInTheDocument()
   })
 })
