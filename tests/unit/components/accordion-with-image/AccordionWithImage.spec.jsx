db614224afa2e16ecdb25ccf43ef5d8fe972b615
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-05-03 23:43:46 +0200

    Added spec for AccordionWithImage component #108

---
+++ tests/unit/components/accordion-with-image/AccordionWithImage.spec.jsx
@@ -0,0 +1,29 @@
+import AccordionWithImage from '~/components/accordion-with-image/AccordionWithImage.jsx'
+import { screen, render, fireEvent } from '@testing-library/react'
+
+import { expect } from 'vitest'
+
+describe('PopupDialog component', () => {
+  it('should show the mock data on click on the title', () => {
+    const items = [
+      {
+        image: 'test/image1',
+        title: 'Test Title1',
+        description: 'Test description1'
+      },
+      {
+        image: 'test/image2',
+        title: 'Test Title2',
+        description: 'Test description2'
+      }
+    ]
+    render(<AccordionWithImage items={items} />)
+
+    const titleElement = screen.findByText(items[0].title)
+    fireEvent.click(titleElement)
+
+    const subtitleElement = screen.findByText(items[0].description)
+
+    expect(subtitleElement).toBeInTheDocument()
+  })
+})
