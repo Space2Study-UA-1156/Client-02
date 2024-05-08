6c536532d49c182d476a4fc1b5031b752a381ac9
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-05-08 23:30:45 +0200

    added image to the subjects step

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,10 +1,21 @@
 import Box from '@mui/material/Box'
 
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
-
+import SubjectsStepImage from '~/assets/img/subjects-step/image.svg'
 const SubjectsStep = ({ btnsBox }) => {
   return (
     <Box sx={styles.container}>
+      <Box
+        alt='Girl studying'
+        component='img'
+        src={SubjectsStepImage}
+        sx={{
+          height: 233,
+          width: 350,
+          maxHeight: { xs: 233, md: 167 },
+          maxWidth: { xs: 350, md: 250 }
+        }}
+      />
       <Box sx={styles.rigthBox}>
         Subjects step
         {btnsBox}
