ab5d3cdb644de38ce0dcad52ec573d9aca113bb4
docia <matiushenko107@gmail.com>
2024-05-08 21:59:25 +0300

    LanguageStep in process

--- src/containers/tutor-home-page/language-step/LanguageStep.jsx
+++ src/containers/tutor-home-page/language-step/LanguageStep.jsx
@@ -1,12 +1,51 @@
+import { Typography } from '@mui/material'
 import Box from '@mui/material/Box'
-
+import image from '~/assets/img/language-step/image.svg'
 import { styles } from '~/containers/tutor-home-page/language-step/LanguageStep.styles'
+import TextField from '@mui/material/TextField'
+import Autocomplete from '@mui/material/Autocomplete'
 
 const LanguageStep = ({ btnsBox }) => {
+  const languageImg = image
+
+  const languages = [
+    'English',
+    'Ukrainian',
+    'Polish',
+    'French',
+    'Spanish',
+    'Arabic'
+  ]
+
   return (
     <Box sx={styles.container}>
-      Language step
-      {btnsBox}
+      <Box sx={styles.imgContainer}>
+        <Box
+          alt='languages'
+          component='img'
+          src={languageImg}
+          sx={styles.img}
+        />
+      </Box>
+      <Box id='form' sx={styles.formContainer}>
+        <Box>
+          <Typography sx={styles.title}>
+            Please select the language in which you would like to study and
+            cooperate.
+          </Typography>
+
+          <Autocomplete
+            disablePortal
+            id='combo-box-demo'
+            options={languages}
+            renderInput={(params) => (
+              <TextField {...params} label='Your native language' />
+            )}
+            sx={{ width: '100%' }}
+          />
+        </Box>
+        <Box>{btnsBox}</Box>
+      </Box>
     </Box>
   )
 }
--- src/containers/tutor-home-page/language-step/LanguageStep.styles.js
+++ src/containers/tutor-home-page/language-step/LanguageStep.styles.js
@@ -4,8 +4,34 @@ export const styles = {
   container: {
     display: 'flex',
     justifyContent: 'space-between',
-    gap: '40px',
+    flexDirection: 'colum',
+    gap: { md: '40px', lg: '63px' },
     height: { sm: '485px' },
     ...fadeAnimation
+  },
+  formContainer: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '432px'
+  },
+  title: {
+    mb: '20px'
+  },
+  // imgContainer: {
+  //   width: '100%',
+  //   maxWidth: { sm: '300', md: '380px', lg: '472px' },
+  //   maxHeight: 'inherit',
+  //   display: { xs: 'none', md: 'flex' }
+  // },
+  imgContainer: {
+    width: '100%',
+    maxWidth: { md: '370px', lg: '472px' },
+    maxHeight: 'inherit',
+    display: { xs: 'none', md: 'flex' }
+  },
+  img: {
+    objectFit: 'contain',
+    width: '100%'
   }
 }
