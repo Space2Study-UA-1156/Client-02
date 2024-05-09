bca1e7282bfd9cb60ff0234d58bf3ed54d8ce840
BodiaPutiak <121493849+BodiaPutiak@users.noreply.github.com>
2024-05-09 23:25:08 +0200

    connected to context

--- src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.jsx
@@ -1,23 +1,95 @@
 import Box from '@mui/material/Box'
+import Typography from '@mui/material/Typography'
+import { useState } from 'react'
+
+import Autocomplete from '@mui/material/Autocomplete'
+import TextField from '@mui/material/TextField'
+import SubjectsStepImage from '~/assets/img/subjects-step/image.svg'
 
 import { styles } from '~/containers/tutor-home-page/subjects-step/SubjectsStep.styles'
-import SubjectsStepImage from '~/assets/img/subjects-step/image.svg'
+import {categoriesMock, languagesMock} from '~/containers/tutor-home-page/subjects-step/constants'
+import AppButton from '~/components/app-button/AppButton'
+import { useStepContext } from '~/context/step-context'
+import AppChipList from '~/components/app-chips-list/AppChipList'
+
 const SubjectsStep = ({ btnsBox }) => {
+  const [category, setCategory] = useState(null)
+  const [subject, setSubject] = useState(null)
+  const { stepData, handleStepData } = useStepContext()
+  const subjectLabel = 'subjects';
+  const selectedSubjects = stepData[subjectLabel]
+
+  const handleChangeCategory = (e, categoryValue) => {
+    setCategory(categoryValue)
+    setSubject(null)
+  }
+
+  const handleChangeSubject = (e, subjectValue) => {
+    setSubject(subjectValue)
+  }
+
+  const handleAddSubject = () => {
+    handleStepData(subjectLabel, [...selectedSubjects, subject])
+    setSubject(null)
+  }
+
+  const handleDeleteSubject = (subjectName) => {
+    handleStepData(
+      subjectLabel,
+      selectedSubjects.filter((subject) => subject.name !== subjectName)
+    )
+  }
+  
   return (
     <Box sx={styles.container}>
       <Box
         alt='Girl studying'
         component='img'
         src={SubjectsStepImage}
-        sx={{
-          height: 233,
-          width: 350,
-          maxHeight: { xs: 233, md: 167 },
-          maxWidth: { xs: 350, md: 250 }
-        }}
+        sx={styles.image}
       />
       <Box sx={styles.rigthBox}>
-        Subjects step
+        <Box sx={styles.titleWithForm}>
+          <Typography>
+            Velit officia consequat duis enim velit mollit. Other categories you can add in your account settings later.
+          </Typography>
+          <Box component='form' sx={styles.form}>
+            <Autocomplete
+              disablePortal
+              id='combo-box-demo'
+              options={categoriesMock}
+              getOptionLabel={(option) => option.name}
+              onChange={handleChangeCategory}
+              renderInput={(params) => (
+                <TextField {...params} label='Main Tutoring Category' />
+              )}
+              sx={styles.inputField}
+            />
+            <Autocomplete
+              disablePortal
+              id='combo-box-demo'
+              options={languagesMock}
+              getOptionLabel={(option) => option.name}
+              onChange={handleChangeSubject}
+              renderInput={(params) => (
+                <TextField {...params} label='Subject' />
+              )}
+              sx={styles.inputField}
+            />
+            <AppButton
+              onClick={handleAddSubject}
+              variant={'tonal'}
+            >
+              Add one more subject
+            </AppButton>
+
+            <AppChipList
+              defaultQuantity={2}
+              handleChipDelete={handleDeleteSubject}
+              items={selectedSubjects.map((subject) => subject.name)}
+            />
+          </Box>
+        </Box>
         {btnsBox}
       </Box>
     </Box>
--- src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
+++ src/containers/tutor-home-page/subjects-step/SubjectsStep.styles.js
@@ -8,5 +8,24 @@ export const styles = {
     height: { sm: '485px' },
     paddingBottom: { xs: '30px', sm: '0px' },
     ...fadeAnimation
+  },
+  image: {
+    height: 'inherit',
+    width: '450px',
+    maxWidth: { md: '50%', lg: '450px' }
+  },
+  titleWithForm: {
+    width: '432px', 
+    display: 'flex', 
+    flexDirection: 'column', 
+    gap: '30px'
+  },
+  form: {
+    display: 'flex', 
+    flexDirection: 'column', 
+    gap: '20px'
+  },
+  inputField: {
+    width: '100%'
   }
 }
