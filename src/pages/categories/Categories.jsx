8a021bc0d013ba38dab3123c16cfa107d1c8d85d
docia <matiushenko107@gmail.com>
2024-05-15 21:58:07 +0300

    create Categories title & search

--- src/pages/categories/Categories.jsx
+++ src/pages/categories/Categories.jsx
@@ -1,7 +1,99 @@
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import { Box, Typography, Paper, InputBase, Autocomplete } from '@mui/material'
+import SearchIcon from '@mui/icons-material/Search'
+import { useTranslation } from 'react-i18next'
+import { styles } from '~/pages/categories/Categories.styles.js'
+import ArrowForwardIcon from '@mui/icons-material/ArrowForward'
+import AppButton from '~/components/app-button/AppButton'
+import HashLink from '~/components/hash-link/HashLink'
+import { categoryService } from '~/services/category-service'
+import { useEffect, useState } from 'react'
+import { authRoutes } from '~/router/constants/authRoutes'
 
 const Categories = () => {
-  return <PageWrapper>Categories</PageWrapper>
+  const [categoriesData, setCategoriesData] = useState()
+
+  const { t } = useTranslation()
+
+  const { categories, subjects, findOffers } = authRoutes
+
+  useEffect(() => {
+    const fetchCategories = async () => {
+      try {
+        const response = await categoryService.getCategories()
+        setCategoriesData(response.data.items)
+      } catch (error) {
+        console.error('Error fetching categories:', error)
+      }
+    }
+
+    fetchCategories()
+  }, [])
+
+  return (
+    <PageWrapper>
+      <Box sx={styles.container}>
+        <Typography sx={styles.title}>{t('categoriesPage.title')}</Typography>
+        <Typography>{t('categoriesPage.description')}</Typography>
+      </Box>
+      <Box sx={styles.showOffersBox}>
+        <Typography
+          component={HashLink}
+          sx={styles.showOffers}
+          to={findOffers.path}
+        >
+          {t('categoriesPage.showAllOffers')}
+          <ArrowForwardIcon sx={styles.arrowIcon} />
+        </Typography>
+      </Box>
+
+      <Paper elevation={0} sx={styles.searchContainer}>
+        <Box sx={styles.searchBox}>
+          <SearchIcon sx={styles.searchIcon} />
+          <Autocomplete
+            freeSolo
+            getOptionLabel={(option) => option.name}
+            options={categoriesData}
+            renderInput={(params) => {
+              const { ...rest } = params
+              return (
+                <InputBase
+                  {...params.InputProps}
+                  {...rest}
+                  placeholder={t('categoriesPage.searchLabel')}
+                  sx={styles.searchInput}
+                />
+              )
+            }}
+            sx={styles.searchBox}
+          />
+          <AppButton sx={styles.searchButton}>Search</AppButton>
+          <AppButton sx={styles.iconButton}>
+            <SearchIcon />
+          </AppButton>
+        </Box>
+      </Paper>
+      <Box sx={styles.underSearchBoxText}>
+        <Typography>{t('categoriesPage.cantFindLabel')}</Typography>
+        <Typography
+          component={HashLink}
+          sx={styles.underlineText}
+          to={categories.path}
+        >
+          {t('categoriesPage.category')}
+        </Typography>
+        <Typography>{t('categoriesPage.and')}</Typography>
+        <Typography
+          component={HashLink}
+          sx={styles.underlineText}
+          to={subjects.path}
+        >
+          {t('categoriesPage.subject')}
+        </Typography>
+        <Typography>{t('categoriesPage.exclmMark')}</Typography>
+      </Box>
+    </PageWrapper>
+  )
 }
 
 export default Categories
--- src/pages/categories/Categories.styles.js
+++ src/pages/categories/Categories.styles.js
@@ -1,1 +1,68 @@
-export const styles = {}
+export const styles = {
+  container: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'column',
+    textAlign: 'center'
+  },
+  title: {
+    fontSize: '32px',
+    fontWeight: '500',
+    mb: '8px'
+  },
+  showOffers: {
+    textDecoration: 'none',
+    color: '#607D8B',
+    alignItems: 'center',
+    my: '20px',
+    display: 'flex',
+    justifyContent: 'flex-end',
+    fontWeight: '500'
+  },
+  arrowIcon: {
+    color: '#607D8B',
+    ml: '10px',
+    width: '20px',
+    height: '20px'
+  },
+  searchIcon: {
+    display: { xs: 'none', sm: 'flex' }
+  },
+  searchContainer: {
+    borderRadius: '50px',
+    py: '25px',
+    px: '44px'
+  },
+  searchBox: {
+    display: 'flex',
+    width: '100%',
+    alignItems: 'center'
+  },
+  searchInput: {
+    ml: '20px',
+    flex: 1
+  },
+  searchButton: {
+    backgroundColor: '#607D8B',
+    ml: '24px',
+    display: { xs: 'none', sm: 'flex' }
+  },
+  iconButton: {
+    backgroundColor: '#607D8B',
+    display: { xs: 'flex', sm: 'none' },
+    ml: '24px'
+  },
+  underSearchBoxText: {
+    display: 'flex',
+    justifyContent: 'center',
+    my: '30px',
+    textAlign: 'center'
+  },
+  underlineText: {
+    color: 'primary.900',
+    mx: '5px',
+    textDecoration: 'underline',
+    fontWeight: '500'
+  }
+}
