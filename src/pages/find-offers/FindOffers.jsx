eb7891b0fdaf19ff1116155c417fd650b7347cc9
Nazarii Lysak <Nazarii.Lysak@ua.nestle.com>
2024-05-24 10:42:47 +0300

    add FindOdders

--- src/pages/find-offers/FindOffers.jsx
+++ src/pages/find-offers/FindOffers.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react'
+
 import PageWrapper from '~/components/page-wrapper/PageWrapper'
+import OffersBlock from '~/containers/offers-block/OffersBlock'
+import { Box } from '@mui/material'
+import ToggleLayoutView from '~/components/toggle-layout-view/ToggleLayoutView'
 
 const FindOffers = () => {
-  return <PageWrapper>Find offers</PageWrapper>
+  const [gridLayout, setGridLayout] = useState(false)
+
+  return (
+    <PageWrapper>
+      <Box>
+        <ToggleLayoutView
+          gridLayout={gridLayout}
+          setGridLayout={setGridLayout}
+        />
+      </Box>
+      <OffersBlock gridLayout={gridLayout} />
+    </PageWrapper>
+  )
 }
 
 export default FindOffers
